25aa6162aa95efc0b2d986db6a4c8c68f8c7d285
Foster Boone <10787343@uvu.edu>
2022-09-07 21:00:49 -0600

    Fix Errors

--- matchgame/assets/Scripts/Vector3DataP.cs
+++ matchgame/assets/Scripts/Vector3DataP.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vector3Data : MonoBehaviour
+public class Vector3DataP : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
