291db634ad322f1500dc8b8ca237f761c18003d1
Zhrine <10787343@uvu.edu>
2022-09-21 21:51:05 -0600

    Count Down works
    No animation tho bc of TMP

--- matchgame/assets/Scripts/CoRoutineBehavior.cs
+++ matchgame/assets/Scripts/CoRoutineBehavior.cs
@@ -4,23 +4,27 @@ using UnityEngine.Events;
 
 public class CoRoutineBehavior : MonoBehaviour
 {
-    public UnityEvent repeatEvent;
+    public UnityEvent startEvent, repeatEvent, endEvent;
     
-    public int counterNum = 3;
+    public IntDataP counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
     IEnumerator Start()
     {
+        
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
         
-        while (counterNum > 0)
+        startEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
         {
-            yield return wfsObj;
             repeatEvent.Invoke();
-            
-            
+            counterNum.value--;
+            yield return wfsObj;
+
         }
+        endEvent.Invoke();
     }
 }
